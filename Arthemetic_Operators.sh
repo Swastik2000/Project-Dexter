284bdadd33af55b7d677a58477d19631508c5397
soumava <soumavadas06@gmail.com>
2022-10-21 22:16:35 +0530

    Added some array, functions and for loop shell scripts

---
+++ Arthemetic_Operators.sh
@@ -0,0 +1,12 @@
+echo -e "Please enter some value: \c"
+read -r a
+echo -e "Please enter another value: \c"
+read -r b
+
+echo "a+b value is $(($a+$b))"    #it will add both the values
+echo "a-b value is $(($a-$b))"    #it will subtract b form a 
+echo "axb value is $(($a*$b))"    #it will multiply both a and b
+echo "a/b value is $(($a/$b))"    #it will divide b from a
+echo "a%b value is $(($a%$b))"    #it will give the remainder when a is divided by b
+
+echo "Completed successfully"
